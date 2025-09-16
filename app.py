7778dbc2744437e3a77ca09e54cb611d7a9de559
MustafaMaska <80588899+MustafaMaska@users.noreply.github.com>
2025-09-16 18:12:37 +0400

    Add requirements.txt and Render start command

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("Home.html", {"request": request})
 
