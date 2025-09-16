cd4a80c86d046f88ea2be358a3e9d8eff64c9fac
Mustafa-Awd <mustafa.h.awad2005@gmail.com>
2025-09-16 13:49:37 +0400

    2nd version initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Serve static files (css, imgs, gifs)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Point to templates folder
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("Home.html", {"request": request})
+
